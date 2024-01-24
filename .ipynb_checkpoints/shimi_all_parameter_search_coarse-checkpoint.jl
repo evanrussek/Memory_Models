b33625d2a29afef838662dba10fd3bdf0e39e79c
Evan Russek <evrussek@gmail.com>
2024-01-23 20:56:43 -0500

    ...

--- .ipynb_checkpoints/shimi_all_parameter_search_coarse-checkpoint.jl
+++ .ipynb_checkpoints/shimi_all_parameter_search_coarse-checkpoint.jl
@@ -7,9 +7,9 @@ run_idx = is_array_job ? parse(Int, ENV["SLURM_ARRAY_TASK_ID"]) : 1
 on_cluster = true
 
 if on_cluster
-    to_save_folder = "/home/erussek/projects/Memory_Models/shimi_all_parameter_search_coarse"
+    to_save_folder = "/home/erussek/projects/Memory_Models/shimi_all_parameter_search_coarse2"
 else
-    to_save_folder = "/Users/erussek/Dropbox/Griffiths_Lab_Stuff/code/Memory_Models/shimi_all_parameter_search_coarse"
+    to_save_folder = "/Users/erussek/Dropbox/Griffiths_Lab_Stuff/code/Memory_Models/shimi_all_parameter_search_coarse2"
 end
 
 mkpath(joinpath(to_save_folder,"exp1"))
@@ -19,15 +19,17 @@ mkpath(joinpath(to_save_folder,"exp3"))
 ## Specify parameters for each job...
 # 21 epsilon values
 
-eps_vals = round.(1 .- collect(.01 .* (1.5 .^ (0:2:10))), digits = 3)
-
+# eps_vals = round.(1 .- collect(.01 .* (1.5 .^ (0:2:10))), digits = 3)
+eps_vals = collect(.99:-.1:.01)
 # quanta values
-q_vals = ceil.(2 .^ collect(1:8))
+# q_vals = ceil.(2 .^ collect(1:8))
 
-mem_slopes = [.0125, .025, .05, .1, .2, .4, .8]
+q_vals = collect(2:10:102)
+
+mem_slopes = [.025, .05, .1, .2, .4, .8]
 
 # re-run with new values so we can see some concavity...
-NT_vals = [12, 25, 50, 100, 200, 400, 800, 1600, 3200] # run w these now... 
+NT_vals = [25, 50, 100, 200, 400, 800, 1600, 3200] # run w these now... 
 
 job_eps = []
 job_q = []
@@ -53,7 +55,7 @@ n_jobs_total = length(job_nt)
 
 println("N_Jobs_Total: $n_jobs_total")
 
-n_jobs_per_run = 11 # should be about 30 minutes...
+n_jobs_per_run = 20 # should be about 30 minutes...
 
 n_runs = Int(ceil(n_jobs_total/n_jobs_per_run))
 println("N_Runs: $n_runs")
@@ -76,15 +78,15 @@ for this_job_idx in these_jobs
     println("Job: $this_job_idx, N_Quanta: $N_Quanta, epsilon: $epsilon, NT_per_Second: $NT_per_Second, mem_slope: $mem_slope")
     local file_name = "N_Quanta_$(N_Quanta)_epsilon_$(epsilon)_NT_per_Second_$(NT_per_Second)_memslope_$(mem_slope).jld2"
     
-    local job_res_1 = sim_exp1(epsilon, N_Quanta, NT_per_Second; mem_slope = mem_slope, return_last_only=false, N_Trials = N_Trials);
+    local job_res_1 = sim_exp1(epsilon, N_Quanta, NT_per_Second; mem_slope = mem_slope, return_last_only=true, N_Trials = N_Trials);
     local full_file_path = joinpath(to_save_folder,"exp1",file_name)
     jldsave(full_file_path; job_res_1)
 
-    local job_res_2 = sim_exp2(epsilon, N_Quanta, NT_per_Second; mem_slope = mem_slope,return_last_only=false, N_Trials = N_Trials);
+    local job_res_2 = sim_exp2(epsilon, N_Quanta, NT_per_Second; mem_slope = mem_slope,return_last_only=true, N_Trials = N_Trials);
     local full_file_path = joinpath(to_save_folder,"exp2",file_name)
     jldsave(full_file_path; job_res_2)
     
-    local job_res_3 = sim_exp3(epsilon, N_Quanta, NT_per_Second; mem_slope = mem_slope, return_last_only=false, N_Trials = N_Trials);
+    local job_res_3 = sim_exp3(epsilon, N_Quanta, NT_per_Second; mem_slope = mem_slope, return_last_only=true, N_Trials = N_Trials);
     local full_file_path = joinpath(to_save_folder,"exp3",file_name)
     jldsave(full_file_path; job_res_3)
     
---
+++ setupPythonPlot.jl
@@ -0,0 +1,23 @@
+using PythonPlot
+
+
+PythonPlot.matplotlib.rcParams["axes.linewidth"]= 1.0
+PythonPlot.matplotlib.rcParams["grid.linewidth"]= 0.8
+PythonPlot.matplotlib.rcParams["lines.linewidth"]= 1.2000000000000002
+PythonPlot.matplotlib.rcParams["lines.markersize"]= 4.800000000000001
+PythonPlot.matplotlib.rcParams["patch.linewidth"]= 0.8
+PythonPlot.matplotlib.rcParams["xtick.major.width"]= 1.0
+PythonPlot.matplotlib.rcParams["ytick.major.width"]= 1.0
+PythonPlot.matplotlib.rcParams["xtick.minor.width"]= 0.8
+PythonPlot.matplotlib.rcParams["ytick.minor.width"]= 0.8
+PythonPlot.matplotlib.rcParams["xtick.major.size"]= 4.80000000000000
+PythonPlot.matplotlib.rcParams["ytick.major.size"]= 4.800000000000001
+PythonPlot.matplotlib.rcParams["xtick.minor.size"]= 3.2
+PythonPlot.matplotlib.rcParams["ytick.minor.size"]= 3.2
+PythonPlot.matplotlib.rcParams["font.size"]= 9.600000000000001
+PythonPlot.matplotlib.rcParams["axes.labelsize"]= 9.600000000000001
+PythonPlot.matplotlib.rcParams["axes.titlesize"]= 9.600000000000001
+PythonPlot.matplotlib.rcParams["xtick.labelsize"]= 8.8
+PythonPlot.matplotlib.rcParams["ytick.labelsize"]= 8.8
+PythonPlot.matplotlib.rcParams["legend.fontsize"]= 8.8
+PythonPlot.matplotlib.rcParams["legend.title_fontsize"]= 9.600000000000001
